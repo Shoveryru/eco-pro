6290b9994f6a9b2d3b50a49e9fce0974d12213fe
Dmitrii Ershov <dmitrii_ershov@epam.com>
2022-02-20 15:35:02 +0300

    First version of application with Dream World: Hard configuration

---
+++ src/js/data.js
@@ -0,0 +1,136 @@
+﻿const data = [
+  {
+    "id": 1,
+    "title": "Плотницкое дело",
+    "era": 1,
+    "exclusion": [1, 3, 11, 20, 23, 31, 32],
+    "iconName": "Carpentry_Icon.png"
+  },
+  {"id": 2, "title": "Композиты", "era": 3, "exclusion": [2, 3, 13, 15, 16], "iconName": "Composites_Icon.png"},
+  {"id": 3, "title": "Заготовка леса", "era": 1, "exclusion": [1, 2, 3, 11, 18], "iconName": "Logging_Icon.png"},
+  {"id": 4, "title": "Производство бумаги", "era": 2, "exclusion": [4, 32], "iconName": "PaperMilling_Icon.png"},
+  {
+    "id": 5,
+    "title": "Продвинутая выпечка",
+    "era": 3,
+    "exclusion": [5, 9, 19, 21, 22],
+    "iconName": "AdvancedBaking_Icon.png"
+  },
+  {
+    "id": 6,
+    "title": "Продвинутая кулинария",
+    "era": 3,
+    "exclusion": [6, 8, 9, 19, 21, 22],
+    "iconName": "AdvancedCooking_Icon.png"
+  },
+  {"id": 7, "title": "Выпечка", "era": 2, "exclusion": [7, 19, 21, 22], "iconName": "Baking_Icon.png"},
+  {
+    "id": 8,
+    "title": "Готовка на костре",
+    "era": 1,
+    "exclusion": [6, 8, 19, 22],
+    "iconName": "CampfireCooking_Icon.png"
+  },
+  {"id": 9, "title": "Готовка", "era": 2, "exclusion": [5, 6, 9, 19, 22], "iconName": "Cooking_Icon.png"},
+  {
+    "id": 10,
+    "title": "Передовая кулинария",
+    "era": 3,
+    "exclusion": [10, 19, 22, 23, 24],
+    "iconName": "CuttingEdgeCooking_Icon.png"
+  },
+  {
+    "id": 11,
+    "title": "Основы инженерного дела",
+    "era": 1,
+    "exclusion": [1, 3, 11, 27, 28, 31, 32],
+    "iconName": "BasicEngineering_Icon.png"
+  },
+  {"id": 12, "title": "Утилизатор", "era": 2, "exclusion": [12], "iconName": "Survivalist_Icon.png"},
+  {"id": 13, "title": "Электроника", "era": 3, "exclusion": [2, 13, 15, 16, 31], "iconName": "Electronics_Icon.png"},
+  {
+    "id": 14,
+    "title": "Промышленность",
+    "era": 3,
+    "exclusion": [13, 14, 15, 16, 30, 31],
+    "iconName": "Industry_Icon.png"
+  },
+  {"id": 15, "title": "Механик", "era": 2, "exclusion": [2, 15, 18, 20, 21, 30, 31], "iconName": "Mechanics_Icon.png"},
+  {
+    "id": 16,
+    "title": "Добыча нефти",
+    "era": 3,
+    "exclusion": [2, 18, 20, 21, 26, 31, 32],
+    "iconName": "OilDrilling_Icon.png"
+  },
+  {"id": 17, "title": "Фермер", "era": 1, "exclusion": [17, 18, 19, 32], "iconName": "Farming_Icon.png"},
+  {"id": 18, "title": "Удобрение", "era": 1, "exclusion": [3, 17, 18, 19, 22], "iconName": "Fertilizers_Icon.png"},
+  {
+    "id": 19,
+    "title": "Собирательство",
+    "era": 1,
+    "exclusion": [5, 6, 7, 8, 9, 10, 18, 19, 21, 27, 32],
+    "iconName": "Gathering_Icon.png"
+  },
+  {"id": 20, "title": "Пчеловод", "era": 2, "exclusion": [1, 20, 31], "iconName": "Survivalist_Icon.png"},
+  {"id": 21, "title": "Мельничное дело", "era": 2, "exclusion": [5, 7, 10, 19, 21], "iconName": "Milling_Icon.png"},
+  {
+    "id": 22,
+    "title": "Разделка животных",
+    "era": 1,
+    "exclusion": [5, 6, 7, 8, 9, 10, 18, 22, 23, 24, 32],
+    "iconName": "Butchery_Icon.png"
+  },
+  {"id": 23, "title": "Животновод", "era": 2, "exclusion": [1, 10, 22, 23], "iconName": "Survivalist_Icon.png"},
+  {"id": 24, "title": "Охотник", "era": 1, "exclusion": [10, 22, 24, 32], "iconName": "Hunting_Icon.png"},
+  {
+    "id": 25,
+    "title": "Продвинутая каменная кладка",
+    "era": 3,
+    "exclusion": [25, 28, 30, 31],
+    "iconName": "AdvancedMasonry_Icon.png"
+  },
+  {
+    "id": 26,
+    "title": "Обработка стекла",
+    "era": 2,
+    "exclusion": [16, 26, 28, 30],
+    "iconName": "Glassworking_Icon.png"
+  },
+  {
+    "id": 27,
+    "title": "Каменная кладка",
+    "era": 1,
+    "exclusion": [11, 19, 27, 28, 29, 30],
+    "iconName": "Masonry_Icon.png"
+  },
+  {
+    "id": 28,
+    "title": "Горное дело",
+    "era": 1,
+    "exclusion": [11, 25, 26, 27, 28, 29, 30, 31],
+    "iconName": "Mining_Icon.png"
+  },
+  {"id": 29, "title": "Гончарство", "era": 2, "exclusion": [27, 28, 29], "iconName": "Pottery_Icon.png"},
+  {
+    "id": 30,
+    "title": "Продвинутая выплавка",
+    "era": 3,
+    "exclusion": [14, 15, 16, 25, 26, 27, 28, 30, 31],
+    "iconName": "AdvancedSmelting_Icon.png"
+  },
+  {
+    "id": 31,
+    "title": "Выплавка",
+    "era": 2,
+    "exclusion": [1, 11, 13, 14, 15, 16, 20, 28, 30, 31],
+    "iconName": "Smelting_Icon.png"
+  },
+  {
+    "id": 32,
+    "title": "Работа с тканью",
+    "era": 1,
+    "exclusion": [1, 4, 11, 16, 17, 19, 22, 24, 32],
+    "iconName": "Tailoring_Icon.png"
+  }
+];
---
+++ src/js/func.js
@@ -0,0 +1,206 @@
+﻿const div = 'div';
+const p = 'p';
+const img = 'img'
+const empty = '';
+const cardClassName = 'p-card';
+const rowClassName = 'row';
+const cardImageClassName = 'p-card-img';
+const colTitleClassName = 'block';
+const cardTitleClassName = 'p-card-body';
+const onclickAttr = 'onclick';
+const insertPosition = 'beforeend';
+
+const emptyArray = 0;
+const firstEraId = 1;
+const firstEraArrayIndex = 0;
+const secondEraId = 2;
+const secondEraArrayIndex = 1;
+const thirdEraId = 3;
+const thirdEraArrayIndex = 2;
+const firstElementArrayIndex = 0;
+
+// Classes name for DOM element generation.
+const firstEraName = 'first-era';
+const secondEraName = 'second-era';
+const thirdEraName = 'third-era';
+const pickedIdName = 'picked';
+const firstEraCardClassName = cardClassName + ' ' + firstEraName;
+const secondEraCardClassName = cardClassName + ' ' + secondEraName;
+const thirdEraCardClassName = cardClassName + ' ' + thirdEraName;
+
+// Func names for DOM element generation.
+const addProfessionFuncName = 'addProfession(this)';
+const deleteProfessionFuncName = 'deleteProfession(this)';
+const clearAllFuncName = 'clearAll()';
+
+// TODO: Change before deploying!
+const baseUrl = '../src/icons/';
+
+// Variables
+let user = [];
+let availableProfessions = [];
+
+
+// ACTION: Add profession to user list and initialize redraw.
+function addProfession(element) {
+  let profession = availableProfessions.find(p => p.id.toString() === element.id)
+  user.push(profession);
+  redrawUserPickedBlock();
+  refreshAvailableProfessions(profession, true);
+  redrawAvailableBlock();
+}
+
+// ACTION: Delete profession from user list and initialize redraw.
+function deleteProfession(element) {
+  let profession = user.find(p => p.id.toString() === element.id)
+  user = user.filter(p => p.id !== profession.id);
+  redrawUserPickedBlock();
+  refreshAvailableProfessions(profession, false);
+  redrawAvailableBlock();
+}
+
+// Start up func aka hello world.
+function initializeApplication() {
+  availableProfessions = data;
+  redrawAvailableBlock();
+}
+
+// Initialize redrawing picked professions by user block.
+function redrawUserPickedBlock() {
+  let sortedProfessions = sortProfessionsByEra(user);
+  let redrawStarted = false;
+
+  sortedProfessions.forEach(function (group) {
+    if (redrawStarted) {
+      refreshProfessionsBlock(group, pickedIdName, false, false);
+    } else {
+      refreshProfessionsBlock(group, pickedIdName, true, false);
+      redrawStarted = true;
+    }
+  });
+}
+
+// Initialize redrawing available professions block.
+function redrawAvailableBlock() {
+  let sortedProfessions = sortProfessionsByEra(availableProfessions);
+  clearAvailableBlockData(sortedProfessions);
+  sortedProfessions.forEach(function (group) {
+    if (group.length === emptyArray) return;
+    let groupEra = group[firstElementArrayIndex]?.era;
+    let targetClassName = getEraClassNameForCard(groupEra, false);
+    refreshProfessionsBlock(group, targetClassName);
+  })
+}
+
+// Clear eras block if available array is null.
+function clearAvailableBlockData(sortedProfessions) {
+  if (sortedProfessions[firstEraArrayIndex].length === emptyArray) getTargetDOMElement(firstEraName, true);
+  if (sortedProfessions[secondEraArrayIndex].length === emptyArray) getTargetDOMElement(secondEraName, true);
+  if (sortedProfessions[thirdEraArrayIndex].length === emptyArray) getTargetDOMElement(thirdEraName, true);
+}
+
+// Sort all professions by era.
+function sortProfessionsByEra(source) {
+  let firstEraList = source.filter(profession => profession?.era === firstEraId).sort(p => p.id);
+  let secondEraList = source.filter(profession => profession?.era === secondEraId).sort(p => p.id);
+  let thirdEraList = source.filter(profession => profession?.era === thirdEraId).sort(p => p.id);
+
+  return [firstEraList, secondEraList, thirdEraList];
+}
+
+// Source - professions list for rendering.
+// Target - Id of DOM element for rendering.
+function refreshProfessionsBlock(source, target, eraseInnerHtml = true, addingFunctionMode = true) {
+  if (target === undefined || target === null ) return;
+  let targetDOMElement = getTargetDOMElement(target, eraseInnerHtml);
+  
+  if (source.length === emptyArray) return;
+  
+  source.forEach(function (profession) {
+    let cardDOMElement = createCard(profession, addingFunctionMode);
+    targetDOMElement.insertAdjacentElement(insertPosition, cardDOMElement);
+  })
+}
+
+// Find DOM element by ID and clear inner HTML.
+function getTargetDOMElement(target, eraseInnerHtml) {
+  let targetDOMElement = document.getElementById(target);
+  if (eraseInnerHtml) {
+    targetDOMElement.innerHTML = empty;
+  }
+  return targetDOMElement;
+}
+
+// Create block with card
+function createCard(profession, addingFunctionMode) {
+  let rootCardDivDOMElement = document.createElement(div);
+
+  let rowDivDOMElement = document.createElement(div);
+
+  let cardImageColDOMElement = document.createElement(div);
+  let cardImageDOMElement = document.createElement(img);
+
+  let cardTitleColDOMElement = document.createElement(div);
+  let cardTitleDivDOMElement = document.createElement(div);
+  let cardTitleParaDOMElement = document.createElement(p);
+
+  cardTitleParaDOMElement.innerText = profession.title;
+  cardTitleDivDOMElement.className = cardTitleClassName;
+  cardTitleDivDOMElement.insertAdjacentElement(insertPosition, cardTitleParaDOMElement);
+  cardTitleColDOMElement.className = colTitleClassName;
+  cardTitleColDOMElement.insertAdjacentElement(insertPosition, cardTitleDivDOMElement);
+
+  cardImageDOMElement.src = createImagePath(profession.iconName);
+  cardImageDOMElement.alt = profession.title;
+  cardImageColDOMElement.className = cardImageClassName;
+  cardImageColDOMElement.insertAdjacentElement(insertPosition, cardImageDOMElement);
+
+  rowDivDOMElement.className = rowClassName;
+  rowDivDOMElement.insertAdjacentElement(insertPosition, cardImageColDOMElement);
+  rowDivDOMElement.insertAdjacentElement(insertPosition, cardTitleColDOMElement);
+
+  rootCardDivDOMElement.className = getEraClassNameForCard(profession.era);
+  rootCardDivDOMElement.setAttribute(onclickAttr, addingFunctionMode ? addProfessionFuncName : deleteProfessionFuncName);
+  rootCardDivDOMElement.id = profession.id;
+  rootCardDivDOMElement.insertAdjacentElement(insertPosition, rowDivDOMElement);
+
+  return rootCardDivDOMElement;
+}
+
+// Return class name depending on item era.
+function getEraClassNameForCard(era, availableRenderingMode = true) {
+  switch (era) {
+    case firstEraId:
+      return availableRenderingMode ? firstEraCardClassName : firstEraName;
+    case secondEraId:
+      return availableRenderingMode ? secondEraCardClassName : secondEraName;
+    case thirdEraId:
+      return availableRenderingMode ? thirdEraCardClassName : thirdEraName;
+    case null:
+      return empty;
+  }
+}
+
+// Return relative path to image.
+function createImagePath(path) {
+  return baseUrl + path;
+}
+
+// Refresh list of available professions depending on onclick action.
+function refreshAvailableProfessions(profession, addingFunctionMode) {
+  if (user.length === emptyArray) {
+    availableProfessions = data;
+    return;
+  }
+
+  if (addingFunctionMode) {
+    profession.exclusion.forEach(function (id) {
+      availableProfessions = availableProfessions.filter(p => p.id !== id);
+    });
+  } else {
+    profession.exclusion.forEach(function (id){
+      let excludedProfession = data.find(p => p.id === id);
+      availableProfessions.push(excludedProfession);
+    });
+  }
+}
