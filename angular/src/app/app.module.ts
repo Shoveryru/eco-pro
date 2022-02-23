21822299989d7ae296577072d4989d13a8363f1b
Dmitrii Ershov <39185330+Shoveryru@users.noreply.github.com>
2022-02-24 01:59:32 +0300

    Добавлен bootstrap для angular. Добавлена иконка лого.

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HeaderModule} from "./header/header.module";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HeaderModule,
+        NgbModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
